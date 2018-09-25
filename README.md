0e030ec8bf0be18f48353260e4881d4da140875a
Jesse Garcia <jeg6187@gmail.com>
2018-09-24 18:24:31 -0600

    finished backend (frontend needs work)

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Node User Manager
 brought to you by a a junior dev learning node
 
+### Before you begin:
+Download and install mongodb (may need some configuration)
+
+start up mongo server: mongod
+start up mongo shell: mongo
+import userdata.bson file to the data base using: 
+
 ### How to Actually run the Dang thing:
 Install the Node Modules by running: ```npm i```
 Start the server script in the directory of the project: ```node server.js```
-In a web browser, open a new tab with the url being localhost and port 3000 [Click Me](localhost:3000) if your lazy (like me :P)
+In a web browser, open a new tab with the url being localhost and port 8080 [Click Me](localhost:8080) if your lazy (like me :P)
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const path = require('path');
 mongoose.connect('mongodb://localhost/userManagement', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -18,35 +19,66 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
-//this is where you add the rest of the code
-
-app.get('/userListing', (req, res) => {
+//GET Requests
+app.get('/', (req, res) => { //userListing Page
+    user.find({}, (err, docs) => {
+        if (err) console.log(err);
+        res.render('userListing', {users: docs});
+    });
 });
 
-app.get('/addUser/', (req, res) => {
-    const newUser = new user();
-    const body = req.body;
-    console.log(newUser);
-    // newUser.age = body.age;
-    // newUser.name = body.name;
-    // newUser.gender = body.gender;
-    // newUser.phone = body.phone;
-    // newUser.address = body.address;
+app.get('/add', (req, res) => { //Add Page
+    res.render('add');
 });
 
-app.get('/edit/:uid', (req, res) => {});
+app.get('/edit/:uid', (req, res) => { //Edit Page
+    user.findOne({_id:uid}, (err, docArray) => {
+        if (err) console.log(err);
+        res.render('edit', {user:docArray[0]});
+    });
+});
 
-app.post('/add/:uid', (req, res) => {});
-
-app.post('/update/:uid', (req, res) => {});
-
-app.post('/delete/:uid', (req, res) => {
+app.get('/delete/:uid', (req, res) => { //Delete request
     let uid = req.params.uid;
     user.findOneAndDelete({'_id': uid}, (err, data) => {
         if (err) return console.log(`Oops! ${err}`);
-        console.log('success');
+        res.redirect('/');
+    });
+});
+
+app.post('/add', (req, res) => { //Add request
+    const newUser = new user();
+    const body = req.body;
+
+    newUser.age = body.age;
+    newUser.name = body.name;
+    newUser.gender = body.gender;
+    newUser.phone = body.phone;
+    newUser.address = body.address;
+
+    newUser.save((err, data) => {
+        if (err) console.error(err);
+        res.redirect('/');
+    });
+});
+
+app.post('/update/:uid', (req, res) => { //update request
+    const uid = req.params.uid;
+    const body = req.body;
+    const updatedUserData = {
+        age: body.age,
+        name: body.name,
+        gender: body.gender,
+        phone: body.phone,
+        address: body.address
+    };
+    
+    user.findOneAndUpdate({_id:uid}, updatedUserData, {new: true}, (err, data) => {
+        if (err) console.log(err);
+        res.redirect('/');
     });
 });
 
